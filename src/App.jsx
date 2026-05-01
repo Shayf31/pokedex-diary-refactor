362cb492f237253994a1733ea6433766b09f6141
Shay Fleming <shayfleming1@gmail.com>
2026-05-01 11:44:39 +0200

    FR006: add comments to pokedex page and app

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold !text-blue-500">
+      <h1 className="text-4xl font-bold text-blue-500">
   Tailwind is working 🎉
 </h1>
     </>
